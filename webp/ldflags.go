032b86d2f3b217718e122e5e2b6c2edb38b6a276
lucas-araoz <araoz_lucas@ne.bah.com>
2024-11-14 18:49:14 -0300

    add cflags and ldflags directives per os

--- webp/ldflags.go
+++ webp/ldflags.go
@@ -2,5 +2,9 @@
 
 package webp
 
+// #cgo linux CFLAGS: -I/usr/local/include
+// #cgo linux LDFLAGS: -L/usr/local/lib
+// #cgo darwin CFLAGS: -I/opt/homebrew/include
+// #cgo darwin LDFLAGS: -L/opt/homebrew/lib
 // #cgo LDFLAGS: -lwebp -lsharpyuv -lm
 import "C"
